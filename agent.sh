c9e1c92916be027aea8c14cbfa408c4ea898d5e4
mickael <mickael@debian>
2019-02-08 17:22:23 +0100

    hiding stdout ssh on server

--- agent.sh
+++ agent.sh
@@ -4,6 +4,7 @@
 id=$1
 log_f="log.tmp"
 center="mickael@172.16.131.74"
+pid=$(ps -u | grep -e "agent.sh $id | agent.tmp.sh $id" | sed -n 1p | cut -d " " -f 4)
 
 if [ "$PASS" = "" ]
 then
@@ -37,9 +38,10 @@ chmod u+x analyze.tmp.sh;
 ./analyze.tmp.sh $log_f;
 scp -o StrictHostKeyChecking=no $log_f $center:log/log-$remote;
 rm $log_f;
-./agent.tmp.sh $id;"
+(./agent.tmp.sh $id &);"
 fi
 
 rm agent.tmp.sh 2> /dev/null
 rm config 2> /dev/null
 rm analyze.tmp.sh 2> /dev/null
+exit
